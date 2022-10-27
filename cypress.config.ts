714265845902fe452c565bf098360918e1f2d78c
l.andradas@qualoom.es+1-at-120711052689 <andradex.js07@gmail.com>
2022-10-27 04:43:10 -0400

    fix: remove video from cypress typo and husky config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    }
+    },
     video: false,
     screenshotOnRunFailure: false
   }
